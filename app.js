5a7b9e669fb3bd64a2d18cbe59c3420ab767af21
WebDev <60782852+nurlanzhumakhan111@users.noreply.github.com>
2020-11-13 21:07:31 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -25,78 +25,166 @@ const tasks = [{
 ];
 
 (function (arrOfTasks) {
-  const objOfTasks = arrOfTasks.reduce((acc, task) => {
-    acc[task._id] = task;
-    return acc
-  }, {})
-  console.log(objOfTasks)
-  const listGroup = document.querySelector('.tasks-list-section .list-group')
-  const form = document.forms['addTask'];
-  const inputTitle = form.elements['title'];
-  const inputBody = form.elements['body'];
-  renderAllTasks(objOfTasks)
-  form.addEventListener('submit', onFormSubmitHandler)
-
-  function renderAllTasks(TasksList) {
-    if (!TasksList) {
-      console.error('Error!')
-    }
-    const fragment = document.createDocumentFragment();
-    Object.values(TasksList).forEach((task) => {
-      const li = listItemTemplate(task)
-      fragment.appendChild(li)
+    const objOfTasks = arrOfTasks.reduce((acc, task) => {
+      acc[task._id] = task;
+      return acc
+    }, {})
+    console.log(objOfTasks)
+    const listGroup = document.querySelector('.tasks-list-section .list-group')
+    const listGroupButton = document.querySelectorAll('.list-group-button')
+    const form = document.forms['addTask'];
+    const inputTitle = form.elements['title'];
+    const inputBody = form.elements['body'];
+    renderAllTasks(objOfTasks)
+    form.addEventListener('submit', onFormSubmitHandler)
+    listGroup.addEventListener('click', clickHandler)
+    listGroupButton.forEach(button => {
+      button.addEventListener('click', renderCertainTasks)
     })
-    listGroup.appendChild(fragment)
 
-  }
+    function renderAllTasks(TasksList) {
+      // listGroup.innerHTML
+      if (!TasksList) {
+        console.error('Error!')
+      }
+      const fragment = document.createDocumentFragment();
+      let tasks = Object.values(TasksList).sort(function (x, y) {
+        return (x.completed === y.completed) ? 0 : x.completed ? 1 : -1;
+      })
+      tasks.forEach((task) => {
+          const li = listItemTemplate(task)
+          fragment.appendChild(li)
+        })
+        listGroup.appendChild(fragment)
+      }
 
-  function listItemTemplate({
-    _id,
-    title,
-    body
-  }) {
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    let button = document.createElement('button');
-    let p = document.createElement('p');
-    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
-    button.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
-    p.classList.add('mt-2', 'w-100');
-    span.textContent = title;
-    button.textContent = 'Delete';
-    p.textContent = body;
-    li.appendChild(span)
-    li.appendChild(button)
-    li.appendChild(p)
-    return li
-  }
+      function renderCertainTasks(e) {
+        listGroup.innerHTML = ''
+        const fragment = document.createDocumentFragment();
+        if (e.target.classList.contains('list-group-button-notcompleted')) {
+          Object.values(objOfTasks).forEach((task) => {
+            if (task.completed === false) {
+              const li = listItemTemplate(task)
+              fragment.appendChild(li)
+            }
+          })
+          listGroup.appendChild(fragment)
+        } else if (e.target.classList.contains('list-group-button-all')) {
+          renderAllTasks(objOfTasks)
+        }
+      }
 
-  function onFormSubmitHandler(e) {
-    e.preventDefault();
-    const titleValue = inputTitle.value;
-    const bodyValue = inputBody.value;
+      function listItemTemplate({
+        _id,
+        title,
+        body,
+        completed
+      }) {
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+        let button = document.createElement('button');
+        let buttonDone = document.createElement('button');
+        let p = document.createElement('p');
+        li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+        li.setAttribute('data-task-id', _id);
+        if (completed) {
+          li.classList.add('complete')
+        }
+        button.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+        button.textContent = 'Delete';
+        buttonDone.classList.add('btn', 'btn-done')
+        buttonDone.textContent = 'Done';
 
-    if (!titleValue || !bodyValue) {
-      alert('Введите данные')
-      return
-    }
+        span.textContent = title;
 
-    const task = createNewTask(titleValue, bodyValue)
-    const listItem = listItemTemplate(task);
-    listGroup.insertAdjacentElement('afterbegin', listItem);
-    form.reset();
-  }
+        p.classList.add('mt-2', 'w-100');
+        p.textContent = body;
+        li.appendChild(span)
+        li.appendChild(button)
+        li.appendChild(buttonDone)
+        li.appendChild(p)
+        return li
+      }
 
-  function createNewTask(title, body) {
-    const newTask = {
-      _id: `task-${Math.random()}`,
-      title,
-      body,
-      completed: false,
-    }
-    objOfTasks[newTask._id] = newTask
-    return {
-      ...newTask
-    }
-  }
-})(tasks);
+      function onFormSubmitHandler(e) {
+        e.preventDefault();
+        const titleValue = inputTitle.value;
+        const bodyValue = inputBody.value;
+
+        if (!titleValue || !bodyValue) {
+          alert('Введите данные')
+          return
+        }
+
+        const task = createNewTask(titleValue, bodyValue)
+        const listItem = listItemTemplate(task);
+        listGroup.insertAdjacentElement('afterbegin', listItem);
+        testArr(objOfTasks)
+        form.reset();
+      }
+
+      function createNewTask(title, body) {
+        const newTask = {
+          _id: `task-${Math.random()}`,
+          title,
+          body,
+          completed: false,
+        }
+        objOfTasks[newTask._id] = newTask
+        return {
+          ...newTask
+        }
+      }
+
+      function deleteTask(id) {
+        const {
+          title
+        } = objOfTasks[id];
+        const isConfirm = confirm(`Are you sure that you want to delete ${title} task?`);
+        if (!isConfirm) return;
+        delete objOfTasks[id];
+        return isConfirm
+      }
+
+      function deleteFromHtml(confirmed, el) {
+        if (!confirmed) return;
+        el.remove()
+        testArr(objOfTasks)
+
+      }
+
+      function clickHandler({
+        target
+      }) {
+        if (target.classList.contains('delete-btn')) {
+          onDeleteHandler(target)
+        } else if (target.classList.contains('btn-done')) {
+          onDoneHandler(target)
+        }
+      }
+
+      function onDeleteHandler(target) {
+        const parent = target.closest('[data-task-id]');
+        const id = parent.dataset.taskId;
+        const confirmed = deleteTask(id);
+        deleteFromHtml(confirmed, parent)
+        testArr(objOfTasks)
+      }
+
+      function onDoneHandler(target) {
+        const parent = target.closest('[data-task-id]');
+        const id = parent.dataset.taskId;
+        objOfTasks[id].completed = !objOfTasks[id].completed;
+        parent.classList.toggle('complete');
+      }
+
+      function testArr(obj) {
+        const empty = document.querySelector('.empty');
+        if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+          empty.style.display = 'block';
+        } else {
+          empty.style.display = 'none';
+
+        }
+      }
+    })(tasks);
